8a16d02f4e0a98eb43744b64624df0835159e711
Nargang Vilmos <vilmos.nargang@edu.bme.hu>
2025-03-20 22:48:10 +0100

    Hozzaadtam a RovarMoveLogicot

--- src/interfaces/GameObjectVisitor.java
+++ src/interfaces/GameObjectVisitor.java
@@ -1,4 +1,13 @@
 package interfaces;
 
+import model.gameobjects.Fonal;
+import model.gameobjects.GombaTest;
+import model.gameobjects.Rovar;
+import model.gameobjects.Spora;
+
 public interface GameObjectVisitor {
+    void visit(Spora spora);
+    void visit(GombaTest gombaTest);
+    void visit(Rovar rovar);
+    void visit(Fonal fonal);
 }
--- src/logic_classes/RovarMoveLogic.java
+++ src/logic_classes/RovarMoveLogic.java
@@ -1,4 +1,60 @@
 package logic_classes;
 
-public class RovarMoveLogic {
+import interfaces.GameObjectVisitor;
+import interfaces.GridVisitor;
+import model.gameobjects.Fonal;
+import model.gameobjects.GombaTest;
+import model.gameobjects.Rovar;
+import model.gameobjects.Spora;
+import model.grid.Grid;
+import model.grid.Lava;
+import model.grid.TektonElem;
+
+import java.util.Arrays;
+
+public class RovarMoveLogic extends DiscoverLogic {
+    TektonElem elem;
+    Lava lava;
+    Fonal fonal;
+    private void clearState(){
+        elem=null; lava=null; fonal=null;
+    }
+    @Override
+    public void visit(Spora spora) {
+
+    }
+    @Override
+    public void visit(GombaTest gombaTest) {
+
+    }
+    @Override
+    public void visit(Rovar rovar) {
+
+    }
+    @Override
+    public void visit(Fonal fonal) {
+        this.fonal = fonal;
+    }
+    @Override
+    public void visit(Lava lava) {
+        this.lava = lava;
+        lava.accept((GameObjectVisitor) this);
+    }
+    @Override
+    public void visit(TektonElem elem) {
+        this.elem=elem;
+    }
+    @Override
+    public boolean canMove(Grid from, Grid neighbour) {
+        if(from==neighbour) return true;
+        if(Arrays.stream(from.getNeighbours()).noneMatch((x)->x==neighbour)){
+            return false;
+        }
+        from.accept((GridVisitor) this);
+
+        if(elem!=null || lava!=null && fonal!=null) {
+            return true;
+        }
+        return false;
+    }
 }
