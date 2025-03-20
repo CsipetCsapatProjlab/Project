1b1fd23710efac6997c5d75b297d8623c039b0f3
Sheepy-9 <siposdominiktamas@proton.me>
2025-03-20 15:23:51 +0100

    Logger v1 + Main sablon

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
+
+        //Pelda sorozat
+        MyLogger.Visit("asd");
+        MyLogger.Visit("asd2");
+        MyLogger.In("Van bab? [y/n]", true); //Valaszt var
+        MyLogger.Return("asd2");
+        MyLogger.In("bab2 created", false); //Nem var valaszt
+        MyLogger.Visit("asd3");
+        MyLogger.Return("asd3");
+        MyLogger.Return("asd");
+
         Tests t = new Tests();
         t.setup();
         t.start();
