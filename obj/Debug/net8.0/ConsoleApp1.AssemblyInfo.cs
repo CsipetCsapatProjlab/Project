6e28750d4c14ac1c52ca53c87a407e74c85828fd
Fauszt Márk <fauszt.mark04@gmail.com>
2025-03-23 23:43:54 +0100

    termeszetesen nem volt meg minden teszt

--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a9c34a13b7ae4c511a58e882079dfbea46aeaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33eb32f03585318db76c1ec101cd5128ad3e0993")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
