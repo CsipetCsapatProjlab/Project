d41484ebee38d3f41e0bff721d31c751dc79b831
Kekecs Máté Krisztián <kekecs.mate@gmail.com>
2025-03-30 15:34:27 +0200

    Elkezdtem a pályán dolgozni

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
+import model.Fungorium;
 
 public class Main {
     public static void main(String[] args) {
-
-        //Pelda sorozat
-        MyLogger.Visit("asd");
-        MyLogger.Visit("asd2");
-        MyLogger.In("Van bab? [y/n]", true); //Valaszt var
-        MyLogger.Return("asd2");
-        MyLogger.In("bab2 created", false); //Nem var valaszt
-        MyLogger.Visit("asd3");
-        MyLogger.Return("asd3");
-        MyLogger.Return("asd");
-
-
-        Tests.newsetup();
-        Tests t = new Tests();/*
-        t.setup();
-        t.start();
-        */
+        Fungorium f = new Fungorium(20,100);
+        f.print();
     }
 }
--- src/model/Fungorium.java
+++ src/model/Fungorium.java
@@ -12,7 +12,35 @@ public class Fungorium {
     JatekMotor motor;
     Grid[][] map;
     List<Tekton> tektons;
+    int sor;
+    int oszlop;
 
+    
+    public Fungorium(int ujsor,int ujoszlop){ //A pályát létrehozó konstruktor meghív minden fügvényt ami ahoz kell hogy a pálya létrejöjjön
+        sor = ujsor;
+        oszlop = ujoszlop;
+        map = new Grid[sor][oszlop];
+    }
+    public void print() {
+        System.out.println("Bent");
+        for (int i = 0; i <= this.oszlop + 1; i++) {
+            System.out.print("0");
+        }
+        System.out.println();
+        for (int i = 0; i < this.sor; i++) {
+            System.out.print("0");
+            for (int j = 0; j < this.oszlop; j++) {
+                //System.out.print(map[i][j]);
+                System.out.print(" ");
+            }
+            System.out.print("0");
+            System.out.println();
+        }
+        for (int i = 0; i <= this.oszlop + 1; i++) {
+            System.out.print("0");
+        }
+        System.out.println();
+    }
 
     public JatekMotor getMotor() {
         return motor;
@@ -26,4 +54,5 @@ public class Fungorium {
     }
     public void setMap(Grid[][] g){map = g;}
     public void setTektons(List<Tekton> t){tektons = t;}
+    
 }
