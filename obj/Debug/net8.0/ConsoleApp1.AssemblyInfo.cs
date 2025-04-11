062993301ec5b0cfb1adafa7b7b016ad096aa9e4
Fauszt Márk <fauszt.mark04@gmail.com>
2025-04-11 09:30:00 +0200

    add scripts

--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33eb32f03585318db76c1ec101cd5128ad3e0993")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dbbccd9eca5189afc4f9a242d82f6f27e9d671f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
