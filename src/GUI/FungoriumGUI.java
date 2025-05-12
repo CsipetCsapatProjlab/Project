7509a0e7432918970860e08c279a72f688fd4f9a
Nargang Vilmos <vilmos.nargang@edu.bme.hu>
2025-05-12 14:54:43 +0200

    Lepes objektum hozzáadva Pálya frissítés String szinezés jatekos alapján

--- src/GUI/FungoriumGUI.java
+++ src/GUI/FungoriumGUI.java
@@ -18,10 +18,71 @@ import model.grid.Grid;
 import model.grid.Lava;
 import model.grid.TektonElem;
 import model.players.Jatekos;
+import model.utils.ColorUtils;
 
 public class FungoriumGUI {
     private final Fungorium fungorium;
-    private final List<PlayerGUI> jatekosokGUI;
+    private List<PlayerGUI> jatekosokGUI;
+    private JButton[][] viewGrid;
+
+    private int rows;
+    private int cols;
+
+    private class Lepes{
+        public int startx=-1,starty=-1,endx=-1,endy=-1;
+        public Move move;
+        boolean isMoveSet=false;
+
+        public void addLepes(int x, int y){
+            if(x<0 || y<0 || x>=rows || y>=cols) throw new IllegalArgumentException("Hibás index");
+            if(!isInBounds(startx,starty)){
+                startx = x;
+                starty = y;
+            }
+            else{
+                if(!isInBounds(endx,endy)){
+                    endx = x;
+                    endy = y;
+                }
+            }
+        }
+        public boolean isInBounds(int x, int y){
+            return x>=0 && x<rows && y>=0 && y<cols;
+        }
+        public void addMove(Move move){
+            this.move = move;
+            isMoveSet=true;
+        }
+        public boolean isDone(){
+            return isInBounds(startx,starty) && isInBounds(endx,endy) && isMoveSet;
+        }
+    }
+
+    private void palyaFrissit(){
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                Grid currentGrid=fungorium.getGrid(y,x);
+                List<GameObject> objs = currentGrid.getGameObject();
+                StringBuilder label = new StringBuilder();
+
+                for (GameObject obj : objs) {
+                    Jatekos gameObjectOwner=obj.getObserver();
+                    var color = jatekosokGUI.stream().filter(e->gameObjectOwner==e.jatekos).findFirst();
+                    Color jatekosColor=color.isPresent()?color.get().colorOfJatekos:Color.BLACK;
+                    label.append(wrapInColor(obj.toStringShort(),jatekosColor));
+                }
+
+                viewGrid[y][x].setText(label.toString());
+                if (currentGrid instanceof Lava) {
+                    viewGrid[y][x].setBackground(Color.RED);
+                } else if (currentGrid instanceof TektonElem) {
+                    viewGrid[y][x].setBackground(Color.LIGHT_GRAY);
+                } else {
+                    viewGrid[y][x].setBackground(Color.WHITE);
+                }
+            }
+        }
+    }
 
     public FungoriumGUI(int sor, int oszlop, List<PlayerGUI> playerGUIs) {
         this.jatekosokGUI = playerGUIs;
@@ -31,8 +92,9 @@ public class FungoriumGUI {
         SwingUtilities.invokeLater(() -> {
             Grid[][] map = fungorium.getMap();
 
-            int rows = map.length;
-            int cols = map[0].length;
+            rows = map.length;
+            cols = map[0].length;
+
 
             JFrame frame = new JFrame("Fungorium Grid");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -58,7 +120,25 @@ public class FungoriumGUI {
             jatekosokGUI.forEach(rightPanel::add);
 
             // --------- KÖZÉPSŐ GRID: pálya gombokkal ---------
-            var centerPanel = new FieldPanel(map);
+            viewGrid = new JButton[rows][cols];
+            JPanel centerPanel = new JPanel();
+            centerPanel.setLayout(new GridLayout(rows, cols));
+            for (int y = 0; y < rows; y++) {
+                for (int x = 0; x < cols; x++) {
+                    Grid currentGrid=map[y][x];
+                    viewGrid[y][x] = new JButton(getLabel(currentGrid));
+                    if (currentGrid instanceof Lava) {
+                        viewGrid[y][x].setBackground(Color.RED);
+                    } else if (currentGrid instanceof TektonElem) {
+                        viewGrid[y][x].setBackground(Color.LIGHT_GRAY);
+                    } else {
+                        viewGrid[y][x].setBackground(Color.WHITE);
+                    }
+                    viewGrid[y][x].setOpaque(true);
+                    viewGrid[y][x].setBorderPainted(true);
+                    centerPanel.add(viewGrid[y][x]);
+                }
+            }
 
             frame.add(leftPanel, BorderLayout.WEST);
             frame.add(centerPanel, BorderLayout.CENTER);
@@ -67,6 +147,23 @@ public class FungoriumGUI {
             frame.pack();
             frame.setVisible(true);
         });
+    }
+
+    private static String getLabel(Grid grid) {
+        if (grid == null) return "";
+        List<GameObject> objs = grid.getGameObject();
+        StringBuilder label = new StringBuilder();
+        for (GameObject obj : objs) {
+            if (obj instanceof Rovar) label.append("R");
+            else if (obj instanceof GombaTest) label.append("G");
+            else if (obj instanceof Spora) label.append("S");
+            else if (obj instanceof Fonal) label.append("F");
+        }
+        return label.toString();
+    }
+
+    private static String wrapInColor(String label, Color color) {
+        return String.format("<font color='%s'>%s</font>", ColorUtils.colorToHex(color),label);
     }
 
     private void addPlayers() {
